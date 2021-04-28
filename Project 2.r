ca2ef989dbfc2a72b72fc4c4ec8fd909ad89cbfe
RishiChalla <rishik.challa@gmail.com>
2021-04-28 12:17:00 -0400

    Filtered the correlates by column

--- Project 2.r
+++ Project 2.r
@@ -1,30 +1,52 @@
 # DTSC 2302 - Project 2
 # Topic: Immigration
 # Group Members: Rishi Challa, Khem Khadka, Joshua Hernandez, Aryan Anerao, Beau Tate
+# NOTE: Information regarding variables, and hypotheses can be found in README.md
 
 library(tidyverse)
 
+# --------------------------------- Setup Variables ---------------------------------
+# Review over these variables manually and set them when running the script
+
+# Whether or not to merge the datasets - set to true only if CleanedData/project2CompleteDataset.csv
+# does not already exist. If you do set this to true, make sure you've already ran `dataCleaning.py`
+performCleaning = FALSE;
+
 # --------------------------------- Data Cleaning ---------------------------------
 
-# Merge all the datasets together
-datasets = read.csv("CleanedData/datasets.csv") # Contains a list of all datasets required to be merged
+# We should perform the data cleaning
+if (performCleaning) {
+	# Merge all the datasets together
+	datasets = read.csv("CleanedData/datasets.csv") # Contains a list of all datasets required to be merged
 
-# First get the correlates data
-mergedData = read.csv("Data/Project2CorrelatesData.csv")
+	# First get the correlates data
+	dataset = read.csv("Data/Project2CorrelatesData.csv")
 
-# Rename the correlates year and state columns since correlates uses a different naming scheme
-names(mergedData)[names(mergedData) == "year"] <- "Year"
-names(mergedData)[names(mergedData) == "state"] <- "State"
+	# Remove all columns except the following who's variables we will use:
+	correlateCols = c("year", "state", "mood", "right2work", "pc_inc_ann", "povrate", "ccpi", "evangelical_pop", "nonwhite", "valueofagsect",
+		"region", "budget_surplus_gsp", "dem_unified", "rep_unified", "pctlatinoleg", "hs_dem_in_sess", "hs_rep_in_sess",
+		"hs_ind_in_sess", "sen_dem_in_sess", "sen_rep_in_sess", "sen_ind_in_sess")
 
-# Loop through and merge all the datasets from the filenames
-for (filename in datasets$Filename) {
-	mergedData = merge(mergedData, read.csv(filename), by = c("State", "Year"))
+	dataset = dataset[correlateCols]
+
+	# Rename the correlates year and state columns since correlates uses a different naming scheme
+	names(dataset)[names(dataset) == "year"] <- "Year"
+	names(dataset)[names(dataset) == "state"] <- "State"
+
+	# Loop through and merge all the datasets from the filenames
+	for (filename in datasets$Filename) {
+		dataset = merge(dataset, read.csv(filename), by = c("State", "Year"))
+	}
+
+	# Save the completed merged dataset as a separate csv
+	write.csv(dataset, file = "CleanedData/project2CompleteDataset.csv", row.names = FALSE)
+} else {
+	# Cleaning has already been performed, just read in the already cleaned file
+	dataset = read.csv("CleanedData/project2CompleteDataset.csv");
 }
 
-# Save the completed merged dataset as a separate csv
-write.csv(mergedData, file = "CleanedData/project2CompleteDataset.csv", row.names = FALSE)
+# --------------------------------- Stepwise Regression ---------------------------------
 
-# --------------------------------- Hypotheses ---------------------------------
 
 
 # --------------------------------- Analysis ---------------------------------
--- README.md
+++ README.md
@@ -2,5 +2,48 @@
 ## Topic: Immigration
 Group Members: Rishi Challa, Khem Khadka, Joshua Hernandez, Aryan Anerao, Beau Tate
 
+Main R script file for analysis is located in `Project 2.r`
+
 ## Data
-All data used for actual analysis is in the `CleanedData/project2CompleteDataset.csv`. Everything else was just used in cleaning. Correlates codebook is located within the `Data/` folder.
+All data used for actual analysis is in the `CleanedData/project2CompleteDataset.csv`. Everything else was just used in cleaning. Correlates codebook is located within the `Data/` folder. We also are using stepwise regression in order to choose certain variables to keep and to remove.
+
+## Variables
+> Note: All variables and analysis was conducted on a state level
+
+All variables listed below can be found in the file `CleanedData/project2CompleteDataset.csv`.
+
+| Variable Name | CSV/Data Frame Variable Name | Included By Stepwise Regression? |
+|---------------|------------------------------|----------------------------------|
+| State | `State` | &check; |
+| Year | `Year` | &check; |
+| Mood Index | `mood` | &check; |
+| Right to Work | `right2work` | &check; |
+| Per Capital Annual Income | `pc_inc_ann` | &check; |
+| State Poverty Rate | `povrate` | &check; |
+| Consumer Price Index | `ccpi` | &check; |
+| Evangelical Population | `evangelical_pop` | &check; |
+| Non-White Population | `nonwhite` | &check; |
+| Value added to Economy by Agriculture Sector | `valueofagsect` | &check; |
+| U.S Region | `region` | &check; |
+| Budget surplus as % of GSP | `budget_surplus_gsp` | &check; |
+| Is there a unified Democratic Government? | `dem_unified` | &check; |
+| Is there a unified Republican Government? | `rep_unified` | &check; |
+| Percentage of Latino Legislators | `pctlatinoleg` | &check; |
+| Number of Democratic Representatives | `hs_dem_in_sess` | &check; |
+| Number of Republican Representatives | `hs_rep_in_sess` | &check; |
+| Number of Nonmajor-Party Representatives | `hs_ind_in_sess` | &check; |
+| Number of Democratic Senators | `sen_dem_in_sess` | &check; |
+| Number of Republican Senators | `sen_rep_in_sess` | &check; |
+| Number of Nonmajor-Party Senators | `sen_ind_in_sess` | &check; |
+| Task Force Agreement | `Task.Force.Agreement` | &check; |
+| Cooperation WIth Detainers | `Cooperation.With.Detainers` | &check; |
+| EVerify Mandates | `EVerify.Mandates` | &check; |
+| Year English Was Declared as Official Lang | `Year.English.Declared` | &check; |
+| Is English Declared as the Official Lang? | `English.Declared` | &check; |
+| Insurance for Unauthorized Children | `Insurance.for.Unauth.Kids` | &check; |
+| Insurance for Unauthorized Adults | `Insurance.for.Unauth.Adults` | &check; |
+| Food Assistance for Unauthorized Children | `Food.Assist.for.Immigrant.Kids` | &check; |
+| Food Assistance for Unauthorized Adults | `Food.Assist.for.Immigrant.Adults` | &check; |
+
+## Hypotheses
+
